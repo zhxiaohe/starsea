ec4c86e83e95f77ee47977840e98162d5abd90b2
zhxiaohe <zhxiaohe@yeah.net>
2017-06-28 09:42:40 +0800

    用户相关函数
    用户密码加密,用户权限验证装饰器等函数

---
+++ cmdb-backend/common/utility.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#coding=utf-8
+from flask import Flask, request, render_template, redirect, url_for, session, g, abort, flash, jsonify
+import hashlib
+from common.token_manage import  Token_Manager
+from functools import wraps
+
+
+def hashpass(src):
+    passwd=hashlib.md5()
+    passwd.update(src)
+    return passwd.hexdigest()
+
+
+
+
+def login_required(func):
+    @wraps(func)
+    def dec(*args,**kwargs):
+        if not  session.get('logged_in'):
+            return redirect(url_for('login'))
+        return func(*args,**kwargs)
+    return dec
+
+
+def login_required_forapi(func):
+    @wraps(func)
+    def dec(*args,**kwargs):
+        if not  session.get('logged_in'):
+            return jsonify({'status': 'error','data': 'please login '})
+        return func(*args,**kwargs)
+    return dec
+
+
+def auth_login_required(func):
+    @wraps(func)
+    def auth(*args,**kwargs):
+        if not request.headers.get('Authorization'):
+            return  'error',401
+        t = request.headers.get('Authorization')
+        au = Token_Manager().verify_auth_token(token=t)
+        if 401 == au:
+            return  'error1',401
+        else:
+            pass
+        return func(*args,**kwargs)
+    return auth
+
